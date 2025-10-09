43d95d82105552f2f37c7844454caa002b00416a
co3chalk <kbs6596@naver.com>
2025-10-09 16:47:42 +0900

    득점 판정 재구성
    4가지 콜라이더 배치로 득점 판정 확인 (태그 추가)
    - 플레이어 사이드
    - 상대 사이드
    - 인코트 여부
    - 언더넷 여부
    
    -> 6가지 조건에 따른 득점 판정을 셔틀콕 프리팹이 Destroy 되기 직전에 판별 후 로그 출력

--- Assets/Scripts/Shuttlecock.cs
+++ Assets/Scripts/Shuttlecock.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
 
 [RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class Shuttlecock : MonoBehaviour
@@ -27,6 +28,13 @@ public class Shuttlecock : MonoBehaviour
     private bool lifeTimerStarted = false; // y<=0.7 이후 타이머 시작 여부
     public bool shootingLock = false;      // 발사 잠금 상태
 
+    // 낙하 지점 영역 판정 용 변수
+    private bool alreadyLanded = false;
+    private bool mySide = false;
+    private bool opponentSide = false;
+    private bool inCourt = false;
+    private bool underNet = false;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -50,6 +58,11 @@ public class Shuttlecock : MonoBehaviour
     void Start()
     {
         // 생성 직후에는 삭제 예약을 하지 않음
+        alreadyLanded = false;
+        mySide = false;
+        opponentSide = false;
+        inCourt = false;
+        underNet = false;
     }
 
     void Update()
@@ -64,6 +77,9 @@ public class Shuttlecock : MonoBehaviour
     private void StartLifeTimer()
     {
         lifeTimerStarted = true;
+
+        // 득점 판정 처리 코루틴 함수
+        StartCoroutine(LandingJudgeRoutine());
 
         // 셔틀콕 삭제 예약
         Destroy(gameObject, lifeTime);
@@ -161,12 +177,71 @@ public class Shuttlecock : MonoBehaviour
         rb.velocity = v;
     }
 
+    private IEnumerator LandingJudgeRoutine()
+    {
+        // OnTriggerEnter 이벤트를 받을 시간 여유
+        yield return new WaitForSeconds(lifeTime - 0.1f);
+
+        // 득점 판정 처리
+        if (underNet)
+        {
+            // 1. 네트 밑 통과 + 플레이어 코트 -> 플레이어 득점
+            if (mySide) UnityEngine.Debug.Log("Player Point");
+            // 2. 네트 밑 통과 + 상대 코트 -> 상대 득점
+            if (opponentSide) UnityEngine.Debug.Log("AI Point");
+        }
+        else
+        {
+            if (mySide)
+            {
+                // 3. 플레이어 영역 + 인코트 -> 상대 득점
+                if (inCourt) UnityEngine.Debug.Log("AI Point");
+                // 4. 플레이어 영역 + 아웃코트 -> 플레이어 득점
+                else UnityEngine.Debug.Log("Player Point");
+            }
+            else if (opponentSide)
+            {
+                // 5. 상대 영역 + 인코트 -> 플레이어 득점
+                if (inCourt) UnityEngine.Debug.Log("Player Point");
+                // 6. 상대 영역 + 아웃코트 -> 상대 득점
+                else UnityEngine.Debug.Log("AI Point");
+            }
+        }
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player")
             && collision.collider is SphereCollider)
         {
-            Debug.Log($"Player SphereCollider 충돌: {collision.collider.name}");
+            UnityEngine.Debug.Log($"Player SphereCollider 충돌: {collision.collider.name}");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // 코트 감지
+        if (other.CompareTag("MySide") && alreadyLanded == false)
+        {
+            alreadyLanded = true;
+            mySide = true;
+            //UnityEngine.Debug.Log("내쪽에 떨어짐");
+        }
+        if (other.CompareTag("OpponentSide") && alreadyLanded == false)
+        {
+            alreadyLanded = true;
+            opponentSide = true;
+            //UnityEngine.Debug.Log("저쪽에 떨어짐");
+        }
+        if (other.CompareTag("InCourt"))
+        {
+            inCourt = true;
+            //UnityEngine.Debug.Log("코트 안에 떨어짐");
+        }
+        if (other.CompareTag("UnderNet"))
+        {
+            underNet = true;
+            //UnityEngine.Debug.Log("네트 밑을 지나감");
         }
     }
 }
